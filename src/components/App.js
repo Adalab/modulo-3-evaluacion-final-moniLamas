e064e5c8a6035ddb7fb91d5b791e34f53505a4bc
monipeny <mlamasrial@gmail.com>
2021-10-01 18:13:45 +0200

    structure html

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,44 @@ import { useState, useEffect } from "react";
 function App() {
   return (
     <>
-      <h1>Rick and Morty</h1>
-      <h2>Characters</h2>
-      <section>
-        <form action="">
-          <label htmlFor="search"></label>
-          <input type="text" id="search" name="search" value />
-        </form>
-      </section>
+      <header className="header">
+        <h1 className="">Rick and Morty</h1>
+        <h2 className="">Characters</h2>
+        <img
+          className="header__img"
+          src="https://help.redbubble.com/hc/article_attachments/360002309526/Rick_and_Morty_-_logo__English_.png"
+          alt="Rick and Morty"
+        />
+      </header>
+      <main className="container">
+        <section className="form">
+          <form action="">
+            <label htmlFor="search" className="form__label">
+              Buscador de personajes
+            </label>
+            <input
+              type="text"
+              id="search"
+              name="search"
+              className="form__input"
+            />
+          </form>
+        </section>
+        <section>
+          <ul className="cards">
+            <li className="card">
+              <a href="">
+                <img className="card__img" src="" alt="" title="" />
+                <h4 className="card__title">Nombre:</h4>
+                <p className="card__text">Especie:</p>
+              </a>
+            </li>
+          </ul>
+        </section>
+      </main>
+      <footer className="footer">
+        <p className="copy">&copy; Rick and Morty 2021</p>
+      </footer>
     </>
   );
 }
