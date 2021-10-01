bd59d01e56d9954c4cf5c40f312d8ee80c2fc8cb
monipeny <mlamasrial@gmail.com>
2021-10-01 16:27:31 +0200

    delete folder data and make new services folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import "../styles/App.scss";
-//import {useState} from "react";
-//import initialData from "../data/contacts.json";
+import { useState, useEffect } from "react";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <h1>Rick and Morty</h1>
+      <h2>Characters</h2>
+      <section>
+        <form action="">
+          <label htmlFor="search"></label>
+          <input type="text" id="search" name="search" value />
+        </form>
+      </section>
+    </>
+  );
+}
 
 export default App;
