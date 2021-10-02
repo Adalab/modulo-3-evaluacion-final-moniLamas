adc26c141c2da8e21761a43a7d03500235b91855
monipeny <mlamasrial@gmail.com>
2021-10-02 14:31:06 +0200

    lifting FilterByName

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,7 +3,10 @@ import FilterByName from "./FilterByName";
 function Filters(props) {
   return (
     <form className="form" action="">
-      <FilterByName />
+      <FilterByName
+        searchName={props.searchName}
+        handleSearchName={props.handleSearchName}
+      />
     </form>
   );
 }
