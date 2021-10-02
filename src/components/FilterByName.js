7b59ee219a36ac03d8c6af9ca986e0aaa3d209ec
monipeny <mlamasrial@gmail.com>
2021-10-02 14:30:50 +0200

    FilterByName

---
+++ src/components/FilterByName.js
@@ -0,0 +1,20 @@
+function FilterByName(props) {
+  return (
+    <>
+      <label htmlFor="search" className="form__label">
+        Buscador
+      </label>
+      <input
+        type="text"
+        id="search"
+        name="search"
+        className="form__input"
+        placeholder="Nombre del personaje"
+        value={props.searchName}
+        onChange={props.handleSearchName}
+      />
+    </>
+  );
+}
+
+export default FilterByName;
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,18 +1,9 @@
+import FilterByName from "./FilterByName";
+
 function Filters(props) {
   return (
     <form className="form" action="">
-      <label htmlFor="search" className="form__label">
-        Buscador
-      </label>
-      <input
-        type="text"
-        id="search"
-        name="search"
-        className="form__input"
-        placeholder="Nombre del personaje"
-        value={props.searchName}
-        onChange={props.handleSearchName}
-      />
+      <FilterByName />
     </form>
   );
 }
