a83d8d83bdc21b601477e71a1381b06c19674060
monipeny <mlamasrial@gmail.com>
2021-10-02 14:55:02 +0200

    fix homePlanet

--- src/services/callToApi.js
+++ src/services/callToApi.js
@@ -1,6 +1,7 @@
 function callToApi() {
   return fetch(
     `https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json`
+    //https://rickandmortyapi.com/api/character
   ).then((response) =>
     response.json().then((json) => {
       return json.results.map((character) => {
@@ -11,7 +12,7 @@ function callToApi() {
           status: character.status,
           species: character.species,
           episodes: character.episode.length,
-          homePlanet: character.origin,
+          homePlanet: character.origin.name,
         };
       });
     })
