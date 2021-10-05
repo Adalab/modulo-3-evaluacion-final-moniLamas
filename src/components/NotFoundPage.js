40354fb6314676b860ba245520eb85343c89bbd3
monipeny <mlamasrial@gmail.com>
2021-10-05 08:51:54 +0200

    finish not found page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,12 +1,16 @@
 import "../styles/components/notFoundPage.scss";
 import image from "../images/5203299.jpg";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
     <div className="notFound">
       <img className="notFound__img" src={image} alt="404" />
       <h2 className="notFound__error">Error: página no encontrada</h2>
-      <p className="notFound__text">Revisa que la dirección de la página</p>
+
+      <Link to="/">
+        <button className="notFound__home">Ir a la home</button>
+      </Link>
     </div>
   );
 };
