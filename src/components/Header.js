40e914b6ecd6e0d384aedc0b142ea9e385cfdb96
monipeny <mlamasrial@gmail.com>
2022-02-16 14:48:11 +0100

    Publish in GitHub Pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
-import "../styles/components/header.scss";
+import '../styles/components/header.scss'
 
 function Header() {
   return (
     <header className="header">
       <img
         className="header__img"
-        src="https://help.redbubble.com/hc/article_attachments/360002309526/Rick_and_Morty_-_logo__English_.png"
+        src="https://occ-0-3380-358.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABcb0yL8KdS3lxlB7w7kD6GfBTivnVVTfpiW0ZQTi6nvwWZIEpS5rU5iFeO_kJxGzEDnOaLx7iq0zM3FJc5e1gGqEGcaCkNotiOAl.png?r=d7f"
         alt="Rick and Morty"
       />
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
